4deb2442d316bbca695bd2073acbc97e49ac4b49
Aaron Na <syn228@nyu.edu>
2018-04-07 22:16:06 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
   if (speed > 60)
-    return "true"
+    return true
     elsif (speed < 40)
-    return "true"
-  else return "false"
+    return true
+  else return false
   end
 end
 
